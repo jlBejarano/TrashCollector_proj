cf14ab0dc68ce3de96365fa4ddf926a664ff41ea
jorge bejarano <jorge.bejarano@yahoo.com>
2020-04-23 11:49:05 -0400

    trash controllers

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using TrashCollector.Models;
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public  string Name { get; set; }
+        public string Address { get; set; }
+
+        [ForeignKey("IdnetityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
