451142123c74626179a1c20d12245aa01a0b407a
jorge bejarano <jorge.bejarano@yahoo.com>
2020-04-23 15:56:38 -0400

    add new models

---
+++ TrashCollector/Models/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public int CityId { get; set; }
+        public int StateId { get; set; }
+        public int ZipCodeId { get; set; }
+    }
+}
---
+++ TrashCollector/Models/TrashCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class TrashCollection
+    {
+        public int TrashCollectionId {get; set;}
+        public string PickUpDay { get; set; }
+        public int StartDate { get; set; }
+        public int EndDate { get; set; }
+    }
+}
